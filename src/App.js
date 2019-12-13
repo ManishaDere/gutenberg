bd70c4a817e0ae8499dcb38b639fc9a35f0df159
ManishaDere <manu.pathare263@gmail.com>
2019-12-13 12:24:12 +0530

    first page done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <div id="gutenberg-intro">
+        <section className="gutenberg-intro__info container">
+          <h1>Gutenberg Project</h1>
+          <p>A social cataloging website that allows you to freely search its database of books, annotations, and reviews.</p>
+        </section>
+      </div>
+      <div id="gutenberg-btns">
+        <section className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Fiction.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">fiction</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Drama.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">drama</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Humour.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">Humour</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Politics.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">Politics</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+            </div>
+            <div className="col-sm-6">
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Philosophy.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">Philosophy</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/History.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">history</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+              <button type="button" className="genre-card">
+                <div className="btn-images__wrapper">
+                  <img src="./images/Adventure.svg" className="btn-images" />
+                </div>
+                <div className="btn-text">adventure</div>
+                <div className="btn-images__wrapper">
+                <img src="./images/Next.svg" className="btn-images" />
+                </div>
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
