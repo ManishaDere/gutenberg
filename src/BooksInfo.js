d9e8e64e378a8fc18710c44edf0ed30ebc22a511
ManishaDere <manu.pathare263@gmail.com>
2019-12-22 09:42:00 +0530

    css changes

--- src/BooksInfo.js
+++ src/BooksInfo.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import qs from 'query-string';
 import axios from 'axios';
+import { NONAME } from 'dns';
 
 class BooksInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   componentDidMount() {
-    axios.get('http://skunkworks.ignitesol.com:8000/books?topic=fiction')
+    let params = qs.parse(this.props.location.search);
+    let topic = params.topic;
+    axios.get('http://skunkworks.ignitesol.com:8000/books?topic=${topic}')
     .then(function(res) {
       console.log("Res==>", res);
     })
   }
   render() {
-    // const value=queryString.parse(this.props.location.search);
-    let params = qs.parse(this.props.location.query);
+    let params = qs.parse(this.props.location.search);
     let topic = params.topic;
-    console.log('params==>', topic)//123
     return (
-      <div className="books-info">
+      <div className="container books-info">
+        <div className="books-info__header">
+          <Link to={{ pathname: "/"}} role="button" className="back-btn">
+            <img src="./images/Back.svg" alt="Back img" />
+            <div className="back-btn__text">{topic}</div>
+          </Link>
+          <div className="books-info__header__search-block">
+            <div className="img-search-block">
+              <img src="/images/Search.svg" alt="search" className="search-icon"/>
+            </div>
+            <input type="search" name="searchTopicAuthor" className="text-box" placeholder="Search"/>
+            <div className="img-cancel-block" style={{ display: "none"}}>
+              <img src="./images/Cancel.svg" alt="cancel" className="cancel-icon"/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
